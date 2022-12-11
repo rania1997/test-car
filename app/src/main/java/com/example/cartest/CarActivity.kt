e10c5376bc8efca878ba9cb2eb970d64800c4c08
rania1997 <rania.benmajdouba@esprit.tn>
2022-12-11 16:23:14 +0100

    km

--- app/src/main/java/com/example/cartest/CarActivity.kt
+++ app/src/main/java/com/example/cartest/CarActivity.kt
@@ -3,25 +3,30 @@ package com.example.cartest
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class CarActivity : AppCompatActivity() {
 
-    var submitButton: Button? = null
+    private var kilometrageEditText: EditText? = null
+    private var submitButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car)
 
+        kilometrageEditText = findViewById(R.id.kilometrageEditText)
         submitButton = findViewById(R.id.submitButton)
+
         submitButton!!.setOnClickListener {
-            startActivity(Intent(this@CarActivity,NotesVoitureActivity::class.java))
+            val intent = Intent(this@CarActivity, NotesVoitureActivity::class.java)
+            intent.putExtra("km", kilometrageEditText!!.text.toString().toInt())
+            startActivity(intent)
         }
+
         val nexButton = findViewById<Button>(R.id.buttonnext)
         nexButton!!.setOnClickListener {
-            startActivity(Intent(this@CarActivity,MapActivity::class.java))
+            startActivity(Intent(this@CarActivity, MapActivity::class.java))
         }
-
-
     }
 }
